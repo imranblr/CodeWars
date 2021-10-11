696292f4c4e85e35ce5d01b1538269736be65cfd
Imran Khan <imrankhan.blr@gmail.com>
2021-10-11 12:14:43 +0300

    2 tests are successful

--- StringsMix.py
+++ StringsMix.py
@@ -25,8 +25,8 @@ def mix(s1, s2):
         # return {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
             return result
 
-    # print(counting([x for x in s1]))
-    # print(counting([x for x in s2]))
+    print(counting([x for x in s1]))
+    print(counting([x for x in s2]))
     l1 = counting([x for x in s1])
     l2 = counting([x for x in s2])
 
@@ -148,33 +148,75 @@ def mix(s1, s2):
 
     def output(l1, l2):
         str1 = ""
+        repeat = []
         for x in list(l1.keys()):
             for y in list(l2.keys()):
-                # print(str1)
-                # print(x, l1[x], y, l2[y])
+
+                print("String ->", str1)
+                print(l1)
+                print(l2)
+                print(x, l1[x], y, l2[y])
                 if l1[x] > l2[y]:
-                    str1 += "1:" + x * l1[x] + "/"
-                    l1.pop(x)
-                    if x in l2.keys():
-                        l2.pop(x)
-                        break
-                elif l2[y] > l1[x]:
-                    str1 += "2:" + y * l2[y] + "/"
-                    l2.pop(y)
-                    if y in l1.keys():
-                        l1.pop(y)
-                        break
-                elif l1[x] == l2[y]:
-                    if x is y:
-                        str1 += "=:" + x * l1[x] + "/"
+
+                    if x not in repeat:
+                        str1 += "1:" + x * l1[x] + "/"
+                        repeat.append(x)
                         l1.pop(x)
+                        break
+                    if x in l2.keys():
+                        repeat.append(x)
+                        l2.pop(x)
+                        #break
+                elif l2[y] > l1[x]:
+                    if y not in repeat:
+                        str1 += "2:" + y * l2[y] + "/"
+                        repeat.append(y)
                         l2.pop(y)
                         break
-                    else:
-                        str1 += "1:" + x * l1[x] + "/"
-                        l1.pop(x)
-                        if x in l2.keys(): l2.pop(x)
-                        break
+                    if y in l1.keys():
+                        repeat.append(y)
+                        l1.pop(y)
+                        #break
+                elif l1[x] == l2[y]:
+                    if ord(y) > ord(x):
+                        if x not in repeat:
+                            str1 += "1:" + x * l1[x] + "/"
+                            repeat.append(x)
+                            l1.pop(x)
+                            if y is list(l2)[-1]:
+                                str1 += "2:" + y * l2[y]
+                            break
+                        if x in l2.keys():
+                            repeat.append(x)
+                            l2.pop(x) #repeat.append(x)
+                            #break
+                    elif ord(x) > ord(y):
+                        if y not in repeat:
+                            str1 += "2:" + y * l2[y] + "/"
+                            repeat.append(y)
+                            l2.pop(y)
+                            if x is list(l1)[-1]:
+                                str1 += "1:" + x * l1[x]
+                            break
+                        if y in l1.keys():
+                            repeat.append(y) #print("Removing Re-Occurances of ->", l1.pop(y))
+                            l1.pop(y)
+                            #break
+                    elif x is y:
+                        # if l1[x] == l1[l1.index(x) + 1] and x not in l2.keys():
+                        #     print("Same values in the list")
+                        # print(l1[l1.index(x) + 1])
+                        if x not in repeat:
+                            str1 += "=:" + x * l1[x] + "/"
+                            repeat.append(x)
+                            l1.pop(x)
+                            repeat.append(y)
+                            l2.pop(y)
+                            break
+
+
+
+
 
                 # if x in l2.keys():
                 #     if x is y:
@@ -194,28 +236,45 @@ def mix(s1, s2):
                 #     if r is 1: str1 += "2:" + x * l1[x] + "/"
                 #     else: str1 += "1:" + x * l1[x] + "/"
                 #     break
-        return str1[:-1]
+        # return str1[:-1]
 
-    # return output(l1, l2)
-    if len(l1) >= len(l2):
-        print("l1 is longer list")
-        return output(l1, l2)
-
-    else:
-        print("l2 is longer list")
-        l1 = counting([x for x in s1], r=1)
-        l2 = counting([x for x in s2], r=1)
-        return output2(l2, l1, r=1)
+        return str1
 
 
+    # if len(l1) >= len(l2):
+    #     print("l1 is longer list")
+    #     return output(l1, l2)
+    #
+    # else:
+    #     print("l2 is longer list")
+    #     l1 = counting([x for x in s1], r=1)
+    #     l2 = counting([x for x in s2], r=1)
+    #     return output2(l2, l1, r=1)
+    # l1 = counting([x for x in s1], r=1)
+    # l2 = counting([x for x in s2], r=1)
+    # return output2(l2, l1, r=1)
+
+    return output(l1, l2)
 
 
 
 
 
-print(mix("Are they hereZaz", "yes, they are here"), "2:eeeee/2:yy/=:hh/=:rr")
-print(mix("Sadus:cpms>orqn3zecwGvnznSgacs", "MynwdKizfd$lvse+gnbaGydxyXzayp"),
-                       '2:yyyy/1:ccc/1:nnn/1:sss/2:ddd/=:aa/=:zz')
+# s1 = "my&friend&Paul has heavy hats! &"
+# s2 = "my friend John has many many friends &"
+# print(mix(s1, s2), "--> 2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss")
+
+# s1 = "mmmmm m nnnnn y&friend&Paul has heavy hats! &"
+# s2 = "my frie n d Joh n has ma n y ma n y frie n ds n&"
+# print(mix(s1, s2), "--> 1:mmmmmm/=:nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss")
+
+s1="Are the kids at home? aaaaa fffff"
+s2="Yes they are here! aaaaa fffff"
+print(mix(s1, s2), "--> =:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh")
+
+# print(mix("Are they hereZaz", "yes, they are here"), "2:eeeee/2:yy/=:hh/=:rr")
+# print(mix("Sadus:cpms>orqn3zecwGvnznSgacs", "MynwdKizfd$lvse+gnbaGydxyXzayp"),
+#                        '2:yyyy/1:ccc/1:nnn/1:sss/2:ddd/=:aa/=:zz')
 # print(mix("looping is fun but dangerous", "less dangerous than coding"),
 #                        "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")
 # print(mix(" In many languages", " there's a pair of functions"),
