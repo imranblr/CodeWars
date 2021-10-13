02f6b473fc8dab5f03eddd457ca1e717e1a50d6a
Imran Khan <imrankhan.blr@gmail.com>
2021-10-13 11:55:43 +0300

    2 tests are successful

--- Tic-Tac-ToeChecker.py
+++ Tic-Tac-ToeChecker.py
@@ -1,26 +1,14 @@
 def is_solved(board):
-    def check(board):
 
-        if board[0][0] == board[1][1] == board[2][2] == 1: return 1
-        if board[0][2] == board[1][1] == board[2][0] == 1: return 1
+    if board[0][0] == board[1][1] == board[2][2] == 1: return 1
+    if board[0][2] == board[1][1] == board[2][0] == 1: return 1
 
-        if board[0][0] == board[1][1] == board[2][2] == 2: return 2
-        if board[0][2] == board[1][1] == board[2][0] == 2: return 2
+    if board[0][0] == board[1][1] == board[2][2] == 2: return 2
+    if board[0][2] == board[1][1] == board[2][0] == 2: return 2
 
-        for x, y, z in board:
-            if x == 0 and x == y and  x == z : return -1
-            elif x == 1 and x == y and y == z : return 1
-            elif x == 2 and x == y and y == z : return 2
-            else: continue
-
-        board_T = []
-        for j in range(len(board)):
-            row = []
-            for i in range(len(board[0])):
-                row.append(board[i][j])
-            board_T.append(row)
+    def check(board, tpos):
         draw = 0
-        for x, y, z in board_T:
+        for x, y, z in board:
             if x == 0 and x == y and  x == z : return -1
             elif x == 1 and x == y and y == z : return 1
             elif x == 2 and x == y and y == z : return 2
@@ -28,9 +16,21 @@ def is_solved(board):
             else: continue
         if draw is 0:
             return draw
-        else:
+        elif tpos is 1:
             return -1
-    return check(board)
+
+    if check(board, tpos=0):
+        return check(board, tpos=0)
+    else:
+        board_T = []
+        for j in range(len(board)):
+            row = []
+            for i in range(len(board[0])):
+                row.append(board[i][j])
+            board_T.append(row)
+        return check(board_T, tpos=1)
+
+
 
 
 
