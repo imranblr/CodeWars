886d3159ea896ebf0b733645045b4a45d382122f
Imran Khan <imrankhan.blr@gmail.com>
2021-10-20 10:24:07 +0300

    sharing work

--- NumberZooPortal.py
+++ NumberZooPortal.py
@@ -10,19 +10,22 @@ def find_missing_number(numbers):
 #     print(mylist)
 #     return "".join(list(str(x) for x in range(1, max(numbers)+1) if x not in mylist))
 #     return list(set(range(mylist[0], mylist[-1])) - set(range(1, mylist[-1])))
-    x = set(range(mylist[0], mylist[-1]+1))
-    y = set(mylist)
-    z = (x-y)
+#     x = set(range(mylist[0], mylist[-1]+1))
+#     y = set(mylist)
+#     z = (x-y)
 #     print(z)
-    if len(z) != 0:
-        return list(z)[0]
+#     if len(z) != 0:
+#         return list(z)[0]
 #         print(list(z)[0])
-    elif 1 in mylist:
-        return int(mylist[-1])+1
+#     elif 1 in mylist:
+#         return int(mylist[-1])+1
 #         print("if 1 is in list", int(mylist[-1])+1)
-    else:
-        return 1
+#     else:
+#         return 1
 #         print("if 1 is not in list")
+    if 1 not in mylist: return 1
+    elif len(mylist) == mylist[-1]: return int(mylist[-1]) + 1
+    else: return list(set(range(1, mylist[-1])) - set(mylist))[0]
 
 print(find_missing_number([2, 3, 4]), 1)
 print(find_missing_number([1, 3, 4]), 2)
