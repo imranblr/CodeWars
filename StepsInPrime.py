44a63e45edf5dd3c49eaced1e8233aa4589885a3
Imran Khan <imrankhan.blr@gmail.com>
2021-11-03 10:26:02 +0300

    sharing work

--- StepsInPrime.py
+++ StepsInPrime.py
@@ -1,32 +1,42 @@
+import math
 def step(g, m, n):
-    prime_list = [2, 3, 5, 7, 11]
-    fprime = False
-    sprime = False
-    for num in range(m, n):
-        #         if [x % y != 0 for y in prime_list]:
-        #         print(num)
-        for i in range(2, num):
-            if (num % i) == 0:
-                # print(num, i)
-                fprime = False
+    mylist = []
+    p = False
+    for pn in range(m, n):
+        for i in range(2, int(math.sqrt(pn))+1):
+            if (pn % i) == 0:
+                p = False
                 break
-            else:
-                fprime = True
-        if fprime is True:
-            for j in range(2, (num + g)):
-                if (num + g) % j == 0:
-                    #                         print(j)
-                    sprime = False
-                    break
+            else: p = True
+        if p is True:
+            mylist.append(pn)
+            for x in mylist:
+                if pn - x == g: return [x, pn]
 
-                else:
-                    sprime = True
-            if sprime is True: return [num, num + g]
 
-#         if x % 5 != 0:
-#                 print(x)
-#                 if (x+g) % 2 !=0 and (x+g) % 3 != 0 and (x+g) % 5 != 0 and (x+g) % 7 != 0 and (x+g) % 11 != 0:
-#                     print(x+g)
+# def step(g, m, n):
+#
+#     fprime = False
+#     sprime = False
+#     for num in range(m, n):
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#                 # print(num, i)
+#                 fprime = False
+#                 break
+#             else:
+#                 fprime = True
+#         if fprime is True:
+#             for j in range(2, (num + g)):
+#                 if (num + g) % j == 0:
+#                     #                         print(j)
+#                     sprime = False
+#                     break
+#
+#                 else:
+#                     sprime = True
+#             if sprime is True: return [num, num + g]
+
 
 print(step(2,100,110), [101, 103])
 print(step(4,100,110), [103, 107])
